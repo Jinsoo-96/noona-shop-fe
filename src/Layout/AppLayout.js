99c931540aaa8bec1b5fd75c8cd0040874afa1fd
신진수 <jins@sinjinsuui-Macmini.local>
2024-11-10 02:12:52 +0900

    css 수정

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router";
 import { Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,8 +11,13 @@ import { getCartQty } from "../features/cart/cartSlice";
 const AppLayout = ({ children }) => {
   const location = useLocation();
   const dispatch = useDispatch();
-
   const { user } = useSelector((state) => state.user);
+
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [navbarHeight, setNavbarHeight] = useState(0);
+  const navbarRef = useRef(null);
+
   useEffect(() => {
     dispatch(loginWithToken());
   }, []);
@@ -22,6 +27,28 @@ const AppLayout = ({ children }) => {
       dispatch(getCartQty());
     }
   }, [user]);
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      setShowNavbar(false);
+    } else {
+      setShowNavbar(true);
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  // Navbar 높이 동적으로 설정
+  useEffect(() => {
+    if (navbarRef.current) {
+      setNavbarHeight(navbarRef.current.offsetHeight);
+    }
+  }, [navbarRef.current]);
+
   return (
     <div>
       <ToastMessage />
@@ -36,8 +63,20 @@ const AppLayout = ({ children }) => {
         </Row>
       ) : (
         <>
-          <Navbar user={user} />
-          {children}
+          {/* Navbar에 ref 추가 */}
+          <div
+            ref={navbarRef}
+            className={`navbar-container ${showNavbar ? "visible" : "hidden"}`}
+          >
+            <Navbar user={user} />
+          </div>
+          {/* Navbar 높이만큼 padding-top 적용 */}
+          <div
+            className="content-with-navbar"
+            style={{ paddingTop: navbarHeight + 20 }}
+          >
+            {children}
+          </div>
         </>
       )}
     </div>
